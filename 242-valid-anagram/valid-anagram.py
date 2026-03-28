3558c649d2551f89ac4f49d797b8f1b4e0053558
SID <65087687+ss-deshmukh@users.noreply.github.com>
2026-03-28 18:05:40 +0530

    Time: 11 ms (75.73%) | Memory: 19.3 MB (86.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        else:
-            s_set, t_set = defaultdict(int), defaultdict(int)
-
-            for i, j in zip(s, t): 
-                s_set[i] += 1
-                t_set[j] += 1
         
-        return s_set == t_set
+        count = defaultdict(int)
+        
+        # One pass to increment and decrement
+        for i, j in zip(s, t):
+            count[i] += 1
+            count[j] -= 1
             
-
-        
-
+        # Check if all values are 0
+        for val in count.values():
+            if val != 0:
+                return False
+                
         return True
